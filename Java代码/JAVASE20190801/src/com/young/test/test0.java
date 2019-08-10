04622b4af1cc16ed7450ae7081c89975636faaab
eternityfantastic <1544152874@qq.com>
2019-08-10 23:34:10 +0800

    java代码

---
+++ Java代码/JAVASE20190801/src/com/young/test/test0.java
@@ -0,0 +1,12 @@
+package com.young.test;
+
+public class test0 {
+	//1.ASCII转化，图形对话框
+	//2。计算Π，Math类
+	//3.金字塔
+	//4.金字塔
+	//5.正反循环，精度比较
+	//6.阶乘求e
+	int a=0x7fffffff;
+	
+}
---
+++ Java代码/JAVASE20190801/src/com/young/test/test07.java
@@ -0,0 +1,22 @@
+package com.young.test;
+
+import java.util.Scanner;
+
+public class test07 {
+	static Scanner input=new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		int randnum=(int)Math.random()%100;//c产生随机数[0.100)
+		while(true)
+		{
+			System.out.println("xianshuru");
+			int a=input.nextInt();
+			
+		}
+		
+		
+		
+		
+	}
+
+}
