ba6bf759b4e94825d01acd4c4c871cacbee1c041
eternityfantastic <1544152874@qq.com>
2019-09-04 23:46:20 +0800

    newday

--- Java代码/Java20190903/src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test implements TestInterface {
-    //int a=4;
-    public void get(){
-        system.out.println("hello world");
-    }
-
-}
--- Java代码/Java20190903/src/TestInterface.java
+++
@@ -1,5 +0,0 @@
-public interface TestInterface {
-//    int a=0;
-//    int b=0;
-   public abstract void get();
-}
