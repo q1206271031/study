76632eb79523436c5824d796b333a7fa58f49869
eternityfantastic <1544152874@qq.com>
2019-09-06 23:08:32 +0800

    add

--- c代码/VScode/GuessingGame.java
+++ c代码/VScode/GuessingGame.java
@@ -1,17 +1,33 @@
+import java.util.*;
+import java.lang.*;
 public class GuessingGame{
-    Scanner in=new Scanner(System.in);
-    int count;//记录猜对字符数量
-    char [] input;//存放用户输入的字符
-    char [] chs;//存放系统产生的字符
-    int result ;//判断结果z
-    int set ;//位置猜对个数；
-z
-z
-    public String RandomChar(){//产生随机字符
+    static Scanner in=new Scanner(System.in);
+    private int count;//记录猜对字符数量
+    private char [] input;//存放用户输入的字符
+    private char [] chs;//存放系统产生的字符
+    private int [] result=new int[2] ;//判断结果z
 
+    public String InputChar(){
+        //chs[];
+    }
+    public static char[] generate(){//产生随机字符
+        char[] letter={'A','B','C','D','E','F','G','H',
+        'I','J','K','L','M','N','O','P','Q','R','S','T','U','V',
+        'W','X','Y','Z'};
+        boolean[] flag=new boolean[letter.length];
+        char[] chs=new char[5];
+        for (int i = 0; i < chs.length; i++) {
+            int index;
+            do{
+                index=(int)(Math.random()*(letter.length));
+                
+            }while(flag[index]==true)
+            chs[i]=letter[index];
+        }
+        return chs;
     }
        
-    public int comp( char [] input,char [] chs){//与玩家输入对比;
+    public int check( char [] input,char [] chs){//与玩家输入对比;
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
                 if (input[i]==chs[j]) {
@@ -29,7 +45,7 @@ z
 
 
     public static void main(){
-        int choose=input.intnext();
+        int choose=in.nextint();
         while(choose){
             if(choose==1){
                 Game();
---
+++ c代码/VScode/Test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+intz,x = 5,y = 10,a = 4,b= 2;
+Z= X++ . --y* b/a; 
+printf(" %d/n",z);
+return 0;
+}
