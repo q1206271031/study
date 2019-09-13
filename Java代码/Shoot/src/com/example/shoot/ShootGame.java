f7f18809d3ce596e2630fb48fe225d39693c0e3b
eternityfantastic <1544152874@qq.com>
2019-09-13 18:10:10 +0800

    add

--- Java代码/Shoot/src/com/example/shoot/ShootGame.java
+++ Java代码/Shoot/src/com/example/shoot/ShootGame.java
@@ -16,19 +16,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ShootGame extends JPanel {
+    public static final int WIDTH = 400; // 面板宽
     public static void main(String[] args) {
-        JFrame frame = new JFrame("初学飞机大战");
+        JFrame frame = new JFrame("初学飞机大战");//窗口
         ShootGame game = new ShootGame(); // 面板对象
         frame.add(game); // 将面板添加到JFrame中
         frame.setSize(WIDTH, HEIGHT); // 设置大小
         frame.setAlwaysOnTop(true); // 设置其总在最上
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 默认关闭操作
-        frame.setIconImage(new ImageIcon("images/icon.jpg").getImage()); // 设置窗体的图标
+        // frame.setIconImage(new ImageIcon("images/icon.jpg").getImage()); // 设置窗体的图标
         frame.setLocationRelativeTo(null); // 设置窗体初始位置
-        frame.setVisible(true); // 尽快调用paint
+        frame.setVisible(true); // 窗户可见性
         game.action(); // 启动执行
     }
-    public static final int WIDTH = 400; // 面板宽
     public static final int HEIGHT = 654; // 面板高
     /** 游戏的当前状态: START RUNNING PAUSE GAME_OVER */
     private int state;
